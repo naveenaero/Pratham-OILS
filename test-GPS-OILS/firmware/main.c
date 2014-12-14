9e35c34b4f95bbdb2cd23303ff89e31b4ffd9463
Naveen Himthani <naveen01.iitb@gmail.com>
2014-12-14 19:16:37 +0530

    adding GPS test code

---
+++ test-GPS-OILS/firmware/main.c
@@ -0,0 +1,134 @@
+/****************************************************************************/
+/* 					PRATHAM - IITB's Student Satellite                      */
+/*                                                                          */
+/* Microcontroller:                                                         */
+/*          Atmel AVRmega128L                                               */
+/* Written for the AVRStudio5 compiler                                      */
+/*                                                                          */
+/* Author:  Hussain Manasawala, MTech in Electronic Systems, IITB           */
+/*                                                                          */
+/* Contact: husainmanasa@ee.iitb.ac.in                                      */
+/*                                                                          */
+/****************************************************************************/
+/*
+ * hMaster_GPStest.c
+ *
+ * Created: 07-04-2012 04:56:21
+ *  Author: Hussain
+ */
+
+#include "common.h"
+#include "uart.h"
+#include "gps.h"
+#include "peripherals.h"
+
+/**
+ * @defgroup global_state Global State parameters
+ */
+//@{
+volatile int8_t GPS_done = -1;
+uint8_t Mode;
+uint64_t Time;
+volatile struct state Current_state;
+//@}
+
+/************************************************************/
+/*				Main begins									*/
+/************************************************************/
+int main(void)
+{
+	//Current_state.gps.x = 1;
+	//Current_state.gps.y = 1;
+	//Current_state.gps.z = 1;
+	//Current_state.gps.v_x = 1;
+	//Current_state.gps.v_y = 1;
+	//Current_state.gps.v_z = 1;
+	//Current_state.gps.year = 1;
+	//Current_state.gps.alt = 1;
+	//Current_state.gps.date = 1;
+	//Current_state.gps.hours = 1;
+	//Current_state.gps.lat = 1;
+	//Current_state.gps.lon = 1;
+	//Current_state.gps.minutes = 1;
+	//Current_state.gps.month = 1;
+	//Current_state.gps.seconds = 1; 
+	//Current_state.gps.time_since_reading = 1; 
+	char array[40];
+	char array1[40];
+	char array2[40];
+	
+	DDRA = 0xF0;
+	PORTA = 0xF0;
+	_delay_ms(1000);
+	PORTA = 0x00;
+	_delay_ms(1000);
+	PORTA = 0xF0;
+	_delay_ms(1000);
+	PORTA = 0x00;
+	_delay_ms(1000);
+	
+	init_UART0();
+  
+	transmit_UART0('\r');
+	transmit_UART0('\r');
+	transmit_UART0('H');
+	transmit_UART0('e');
+	transmit_UART0('l');
+	transmit_UART0('l');
+	transmit_UART0('o');
+	
+	sprintf(array,"\t..This is IITB's Student Satellite...\r");
+	transmit_string_UART0(array);
+
+	sprintf(array1,"\tThis is HUSSAIN's OBC-Master code...");
+	sprintf(array2,"\rCurrent GPS state is =\t");
+		
+/************************************************************/
+	init_UART_GPS();
+	
+	///* Switch on Global interrupts
+	sei();
+  
+/************************************************************/
+	while(1){
+	PORTA = 0xA0;
+	_delay_ms(500);
+	PORTA = 0x50;
+	_delay_ms(500);
+	
+//	transmit_UART0('\r');
+//	transmit_UART0('\r');
+//	transmit_UART0('H');
+//	transmit_UART0('i');
+	
+//	transmit_string_UART0(array1);
+//	transmit_string_UART0(array2);
+	
+/************************************************************/
+	PORTA = 0xA0;
+	_delay_ms(500);
+	PORTA = 0x50;
+	_delay_ms(500);
+	
+	///* * Reading GPS
+    read_GPS();
+    while(UCSR0B & _BV(RXCIE0));
+//	send_preflight((char *)&Current_state.gps, sizeof(struct GPS_reading));
+	sprintf(array,"x = %lu \n",Current_state.gps.x);transmit_string_UART0(array);
+	sprintf(array,"y = %lu \n",Current_state.gps.y);transmit_string_UART0(array);
+	sprintf(array,"z = %lu \n",Current_state.gps.z);transmit_string_UART0(array);
+
+//	sprintf(array,"x = %x\n",Current_state.gps.x);transmit_string_UART0(array);
+//	sprintf(array,"y = %x\n",Current_state.gps.y);transmit_string_UART0(array);
+//	sprintf(array,"z = %x\n",Current_state.gps.z);transmit_string_UART0(array);	
+//	sprintf(array,"%lu ",Current_state.gps.v_x);transmit_string_UART0(array);
+//	sprintf(array,"%lu ",Current_state.gps.v_y);transmit_string_UART0(array);
+//	sprintf(array,"%lu ",Current_state.gps.v_z);transmit_string_UART0(array);
+	
+/************************************************************/
+	}
+	return 0;
+}
+/************************************************************/
+/*				Main ends									*/
+/************************************************************/
