e41329d8b0b69e4af37678b882fe477d6a133159
Naveen Himthani <naveen01.iitb@gmail.com>
2014-12-15 15:37:52 +0530

    modified Magmeter receive code

--- test-Magmeter_receive-OILS/firmware/main.c
+++ test-Magmeter_receive-OILS/firmware/main.c
@@ -21,6 +21,34 @@
 #include "uart.h"
 #include "mag.h"
 #include "peripherals.h"
+#include "timer-oils.h"
+
+volatile uint8_t tot_overflow;
+
+void timer_init(void)
+{
+    // set up timer with prescaler = 256
+    TCCR1B |= (1 << CS11);
+    
+    // initialize counter
+    TCNT1 = 0;
+    
+    // enable overflow interrupt
+    TIMSK |= (1 << TOIE1);
+    
+    // enable global interrupts
+    sei();
+    
+    // initialize overflow counter variable
+    tot_overflow = 0;
+}
+
+ISR(TIMER1_OVF_vect)
+{
+    // keep a track of number of overflows
+    tot_overflow++;
+    
+}
 
 /************************************************************/
 /*				Main begins									*/
@@ -72,7 +100,8 @@ int main(void)
     /// Initialise UART1 for Magnetometer Reception of Data and Transmission of Poll
      init_UART_MM();
     /// Configure the torquer to output the required current values
-     configure_torquer();
+//     configure_torquer();
+    
     
     
     /// Define 3 strings for storing Magnetometer field values
@@ -80,56 +109,65 @@ int main(void)
      char sy[20];
      char sz[20];
     
+    /// Initialise the Timer
+    timer_init();
+    
     /// Start while loop
 	   while (1)
        {
-          
-//           send_MM_cmd("*00P\r");
+            if(tot_overflow >= 15)         // find out what the value of x will be for a delay of 2 seconds
+            {
+               send_MM_cmd("*00P\r");
 
-           /// Receive and store Bx,By,Bz
-           Bx=(int16_t)receive_MM();
-           Bx=(Bx<<8);
-           Bx &= 0xFF00;
-           Bx|=(int16_t)receive_MM();
-           
-           By=(int16_t)receive_MM();
-           By=(By<<8); By &= 0xFF00;
-           By|=(int16_t)receive_MM();
-           
-           Bz=(int16_t)receive_MM();
-           Bz=(Bz<<8);
-           Bz &= 0xFF00;
-           Bz|=(int16_t)receive_MM();
-           
-           /// Receive carriage return and ignore
-           receive_MM();
-           
-           /// Transmit Carriage return
-           transmit_UART0('\r');
-           
-           if (Bx != 0x00 || By != 0x00 || Bz != 0x00)
-           {
- 
-           /// Copy Bx,By,Bz into Strings for transmiting
-           sprintf(sx,"%d",Bx);
-           sprintf(sy,"%d",By);
-           sprintf(sz,"%d",Bz);
-           
-           transmit_UART0('x');
-           transmit_string_UART0(sx);
-           transmit_UART0(' ');
-           
-           transmit_UART0('y');
-           transmit_string_UART0(sy);
-           transmit_UART0(' ');
-           
-           transmit_UART0('z');
-           transmit_string_UART0(sz);
-           transmit_UART0('\r');
-           
-           transmit_string_UART0("Hello this is Naveen from IITB");
-           transmit_UART0('\r');
-           }
+               /// Receive and store Bx,By,Bz
+               Bx=(int16_t)receive_MM();
+               Bx=(Bx<<8);
+               Bx &= 0xFF00;
+               Bx|=(int16_t)receive_MM();
+               
+               By=(int16_t)receive_MM();
+               By=(By<<8); By &= 0xFF00;
+               By|=(int16_t)receive_MM();
+               
+               Bz=(int16_t)receive_MM();
+               Bz=(Bz<<8);
+               Bz &= 0xFF00;
+               Bz|=(int16_t)receive_MM();
+               
+               /// Receive carriage return and ignore
+               receive_MM();
+               
+               /// Transmit Carriage return
+               transmit_UART0('\r');
+               
+               if (Bx != 0x00 || By != 0x00 || Bz != 0x00)
+               {
+     
+                   /// Copy Bx,By,Bz into Strings for transmiting
+                   sprintf(sx,"%d",Bx);
+                   sprintf(sy,"%d",By);
+                   sprintf(sz,"%d",Bz);
+                   
+                   transmit_UART0('x');
+                   transmit_string_UART0(sx);
+                   transmit_UART0(' ');
+                   
+                   transmit_UART0('y');
+                   transmit_string_UART0(sy);
+                   transmit_UART0(' ');
+                   
+                   transmit_UART0('z');
+                   transmit_string_UART0(sz);
+                   transmit_UART0('\r');
+                   
+                   transmit_string_UART0("Hello this is Naveen from IITB");
+                   transmit_UART0('\r');
+               
+               }
+                
+                tot_overflow = 0;
+                TCNT1 = 0;
+            }
            
            
        }
